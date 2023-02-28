02c692efca470267e97e71ceff6053d977b05f37
cyavictor88 <100557319+cyavictor88@users.noreply.github.com>
2023-02-28 12:45:08 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter,HashRouter } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { AuthProvider } from "./hooks/useAuth";
 import { indigo,blue } from "@mui/material/colors";
